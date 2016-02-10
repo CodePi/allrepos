c7b97bfe4cd8afc43e8697baccfe784f22edc4fa
pilardi <CodePi@users.noreply.github.com>
2016-02-10 02:57:17 -0500

    fixed tabs

--- all.sh
+++ all.sh
@@ -4,11 +4,11 @@ echo allrepos
 git $*
 
 for D in */ ; do
-		if [ -d $D/.git ]; then
-				echo "*******************"
-				echo $D
-				cd $D
-				git $*
-				cd ..
-		fi
+    if [ -d $D/.git ]; then
+	echo "*******************"
+	echo $D
+	cd $D
+	git $*
+	cd ..
+    fi
 done
