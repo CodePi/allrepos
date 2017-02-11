e31bbdadb5be506eb167d4b1cb67c38bc27f9ef4
CodePi <CodePi@users.noreply.github.com>
2017-02-10 22:52:48 -0500

    changed ThreadedQueue to ThreadSafeQueue

--- scripts/cloneall.sh
+++ scripts/cloneall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOS=(Configurator MultikeyMap ResultsMap Serialator ThreadedQueue)
+REPOS=(Configurator MultikeyMap ResultsMap Serialator ThreadSafeQueue)
 
 if [ "$1" = "https" ]; then
     URL="https://github.com/CodePi"
