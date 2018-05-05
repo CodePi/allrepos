4815660c8c95df44fa223afeb10a8887c198f448
CodePi <CodePi@users.noreply.github.com>
2018-05-04 21:19:38 -0400

    -

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 for D in */ ; do
-    if [ -d $D/.git ]; then
-	echo "*******************"
-	echo $D
-	cd $D/tests
-	make -j4 $*
-	cd ../..
+    if [ "$D" != "scripts/" ]; then
+      echo "*******************"
+      echo $D
+      cd $D/tests
+      make -j4 $*
+      cd ../..
     fi
 done
