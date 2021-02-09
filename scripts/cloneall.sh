07aebf109bc734b08bb32e1d30e23964dec84e0a
CodePi <CodePi@users.noreply.github.com>
2021-02-09 00:22:50 -0500

    added Units

--- scripts/cloneall.sh
+++ scripts/cloneall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOS=(Configurator ConfiguratorJson MultikeyMap ResultsMap Serialator ThreadSafeQueue)
+REPOS=(Configurator ConfiguratorJson MultikeyMap ResultsMap Serialator ThreadSafeQueue Units)
 
 if [ "$1" = "https" ]; then
     URL="https://github.com/CodePi"
